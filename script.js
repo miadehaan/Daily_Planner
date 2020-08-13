ec088e2bfbf4ea34df98954f473630689000b9a3
miadehaan <64979457+miadehaan@users.noreply.github.com>
2020-08-12 23:17:35 -0700

    all functionality is working and css styles are fixed

--- script.js
+++ script.js
@@ -1,43 +1,76 @@
-var todaysDate = $("#todaysDate");
-var hourBlock = $(".hourBlock");
-var taskField = $(".taskField");
-var newInput;
+$(document).ready(function() {
 
-currentDate();
-
-// Use Moment.js library for dates & time
-function currentDate() {
-    let m = moment(); 
-    console.log(m);
-}
-
-
-// Create each hour block 9am - 5pm
-function hour() {
-
-}
-
-
-
-// Current Hour - Background Color Change
-function currentHour(){
-    // if () {
-
-    // }
-    // else if () {
-
-    // }
-    // else {
-
-    // }
-}
-
-// EventListener for when the taskField is clicked on, and input form shows up
-function newTask() {
+    var todaysDate = $("#todaysDate");
+    var saveBtn = $(".saveBtn");
+    var taskField = $(".taskField");
     
-    // EventListener for when the taskField is clicked on
-        newInput = $("<input>");
-        newInput.placeholder("add new item");
-        $(taskField).append(newInput);
+    thisDate(); // Writes the current date in the header
 
-}
+    //Writes the current date in the header
+    function thisDate () {
+        todaysDate.html(moment().format("MMM Do YYYY"));
+    }
+    //Set Interval to check
+    var dateInterval = setInterval(thisDate, 5000);
+
+
+    // Current Hour - Background Color Change Past/Future/Present
+    function thisHour(){
+        var currentHour = moment().hour(); //grabs current hour in military time
+        // console.log("Current Hour: " + currentHour);
+
+        $(".time-block").each(function(){
+            var blockHour = parseInt($(this).children(".hourBlock").attr("id"));
+            // console.log("Block Hour: " + blockHour);
+
+            if (blockHour < currentHour) {
+                $(taskField).addClass("past");
+                $(taskField).removeClass("present");
+
+                console.log("past");
+            }
+            else if (currentHour === blockHour) {
+                $(taskField).addClass("present");
+
+                console.log("present");
+            }
+            else {
+                $(taskField).addClass("future");
+                $(taskField).removeClass("present");
+                $(taskField).removeClass("past");
+
+                console.log("future");
+            }
+        })
+
+    }
+    //Set on interval
+    var interval = setInterval(thisHour, 1000);
+
+    // EventListener for when a specific taskField is clicked on, and input form shows up
+    $(saveBtn).on("click", function () {
+        // When the taskField is clicked on...
+
+        var value = $(this).siblings(".taskField").val();
+        var time = $(this).siblings(".hourBlock").attr("id");
+        console.log(time, value);
+
+        localStorage.setItem(time, value);
+
+    });
+
+    //The grabs the value from the localStorgage corresponding to the hour
+    $("#8").siblings(".taskField").val(localStorage.getItem("8"));
+    $("#9").siblings(".taskField").val(localStorage.getItem("9"));
+    $("#10").siblings(".taskField").val(localStorage.getItem("10"));
+    $("#11").siblings(".taskField").val(localStorage.getItem("11"));
+    $("#12").siblings(".taskField").val(localStorage.getItem("12"));
+    $("#13").siblings(".taskField").val(localStorage.getItem("13"));
+    $("#14").siblings(".taskField").val(localStorage.getItem("14"));
+    $("#15").siblings(".taskField").val(localStorage.getItem("15"));
+    $("#16").siblings(".taskField").val(localStorage.getItem("16"));
+    $("#17").siblings(".taskField").val(localStorage.getItem("17"));
+
+
+
+});
